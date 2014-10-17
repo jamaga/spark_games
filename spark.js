7e8470c9b23c7e63ad29d891cb6a4278e682d55f
Karolinka <li-nee@gmx.de>
2014-10-17 13:11:23 +0200

    location works in Safari

--- spark.js
+++ spark.js
@@ -2,7 +2,7 @@ var datafromServer = {};
 var apicall = function(){
 	TinderAjax.getJSON("http://ironhack-meet-me-api.herokuapp.com/locations.json", function(data) {
    	console.log(data);
-   	v.querySelector("#ajax").after("Success");
+   	v.querySelector("#ajax").after(" Success!!!!");
    	datafromServer = data;
 	});
 };
@@ -11,18 +11,27 @@ var v = new TinderQuery();
 v.querySelector("#ajax").on('click', apicall);
 v.querySelector("#save").on('click', saveDatainlocalStorage);
 v.querySelector("#populate").on('click', populateList);
+v.querySelector("#geolocate").on('click', geolocateMe);
 
 function saveDatainlocalStorage() {
 	var storage = TinderStorage.getInstance();
 	for ( var i = 0; i < datafromServer.locations_list.length; i++ ){
   	storage.create( datafromServer.locations_list[i].name , datafromServer.locations_list[i]);
   }
-  v.querySelector("#save").after("Success");
+  v.querySelector("#save").after(" Success!!!!");
 }
 
 function populateList(){
 	for ( var i = 0; i < datafromServer.locations_list.length; i++ ){
 		v.querySelector("ul").append("<li>" + datafromServer.locations_list[i].name + "</li>");
 	}
+  v.querySelector("#populate").after(" Success!!!!");
 }
 
+function geolocateMe() {
+	var myPosition = new TinderGeolocation();
+	myPosition.geolocate(function(coords){
+		console.log(coords);
+	});
+  v.querySelector("#geolocate").after(" Success!!!!");
+}
