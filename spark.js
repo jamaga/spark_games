ccbe093511a1a3154f88e29d5585d588961ec54a
Karolinka <li-nee@gmx.de>
2014-10-17 15:53:36 +0200

    made notification

--- spark.js
+++ spark.js
@@ -1,4 +1,7 @@
 var datafromServer = {};
+var locationHere = "";
+var longitude;
+var latitude;
 var apicall = function(){
 	TinderAjax.getJSON("http://ironhack-meet-me-api.herokuapp.com/locations.json", function(data) {
    	console.log(data);
@@ -12,6 +15,8 @@ v.querySelector("#ajax").on('click', apicall);
 v.querySelector("#save").on('click', saveDatainlocalStorage);
 v.querySelector("#populate").on('click', populateList);
 v.querySelector("#geolocate").on('click', geolocateMe);
+v.querySelector("#notify").on('click', addressInfo);
+
 
 function saveDatainlocalStorage() {
 	var storage = TinderStorage.getInstance();
@@ -30,8 +35,34 @@ function populateList(){
 
 function geolocateMe() {
 	var myPosition = new TinderGeolocation();
-	myPosition.geolocate(function(coords){
-		console.log(coords);
-	});
+	// myPosition.geolocate(function(coords){
+		
+		latitude = 41.39606733017981;
+		longitude = 2.193755822002537;
+		locationHere = "Latitude:" + latitude + " Longitude:"+ longitude;
+
+	// });
   v.querySelector("#geolocate").after(" Success!!!!");
 }
+
+function addressInfo() {
+	TinderAjax.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude , function(data) {
+ 	var addressDetails =  data.results[0].formatted_address; 
+ 	NotifyMyLocation(addressDetails);  
+	})
+}
+
+function NotifyMyLocation (addressDetails) {
+	var options = {
+	       icon: "http://lorempixel.com/60/60",
+	     	 body: addressDetails
+	     }
+	var notification = TinderNotification.getInstance();
+	notification.notificate(locationHere, options);
+	console.log("located!");
+	v.querySelector("#notify").after(" Success!!!!");
+}
+
+
+
+
