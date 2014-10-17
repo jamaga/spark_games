9eec973ba2d85e96e2f3923d12471f3649e0e499
Karolinka <li-nee@gmx.de>
2014-10-17 12:06:35 +0200

    saved data in local storage

--- spark.js
+++ spark.js
@@ -1,8 +1,20 @@
+var datafromServer = {};
 var apicall = function(){
 	TinderAjax.getJSON("http://ironhack-meet-me-api.herokuapp.com/locations.json", function(data) {
    	console.log(data);
+   	datafromServer = data;
 	});
-}
+};
 
 var v = new TinderQuery();
 v.querySelector("#ajax").on('click', apicall);
+v.querySelector("#save").on('click', saveDatainlocalStorage);
+
+function saveDatainlocalStorage() {
+	var storage = TinderStorage.getInstance();
+	for ( var i=0; i < datafromServer.locations_list.length; i++ ){
+  	storage.create( i , datafromServer.locations_list[i]);
+  }
+}
+
+
